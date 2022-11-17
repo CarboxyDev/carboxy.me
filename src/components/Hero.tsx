028d71e652556814ab1bb6272666dc5421609322
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-11-17 18:37:40 +0530

    Add illustration for large+ screens to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div>
-      <div className="hero min-h-screen grid md:hidden">
+      <div className="hero min-h-screen grid lg:hidden">
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-6xl font-bold font-quicksand text-slate-50">
@@ -16,17 +16,26 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="hero min-h-screen hidden md:grid">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-6xl font-bold font-quicksand text-slate-50">
-              I build websites
-            </h1>
-            <div className="py-12 text-2xl text-[#A1A1A1] font-light font-nunito">
-              <p>Hey. I'm CarboxyDev</p>
-              <p className="p-1">Full stack & UI/UX Developer</p>
+      <div className="hero min-h-screen hidden lg:grid">
+        <div className="flex flex-row w-full h-full">
+          <div className="hero-content flex-1">
+            <div className="max-w-md">
+              <h1 className="text-6xl font-bold font-quicksand text-slate-50">
+                I build websites
+              </h1>
+              <div className="py-12 text-2xl text-[#A1A1A1] font-light font-nunito">
+                <p>Hey. I'm CarboxyDev</p>
+                <p className="pt-1">Full stack & UI/UX Developer</p>
+              </div>
+              <button className="btn btn-primary">About Me</button>
             </div>
-            <button className="btn btn-primary">About Me</button>
+          </div>
+          <div className="hero-graphic flex-1 flex">
+            <img
+              className="h-64 w-64 mx-auto self-center"
+              src="/hero-illustration.svg"
+              alt="hero-illustration"
+            />
           </div>
         </div>
       </div>
