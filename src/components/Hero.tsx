76bb49c817713c36d388e090bbbb915e5b931544
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-11-17 17:16:32 +0530

    Revert to old globals.css file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
-    <>
-      <div className="hero min-h-screen">
+    <div>
+      <div className="hero min-h-screen grid md:hidden">
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-6xl font-bold font-quicksand text-slate-50">
@@ -15,7 +15,22 @@ const Hero = () => {
           </div>
         </div>
       </div>
-    </>
+
+      <div className="hero min-h-screen hidden md:grid">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-6xl font-bold font-quicksand text-slate-50">
+              I build websites
+            </h1>
+            <div className="py-12 text-2xl text-[#A1A1A1] font-light font-nunito">
+              <p>Hey. I'm CarboxyDev</p>
+              <p className="p-1">Full stack & UI/UX Developer</p>
+            </div>
+            <button className="btn btn-primary">About Me</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
