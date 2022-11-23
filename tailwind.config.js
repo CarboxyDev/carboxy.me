9572ece2db988e61f1f1c1e01f25fb54cb718ed8
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-11-23 23:03:55 +0530

    Add @tailwindcss/forms for opinionated basic pre-made form components

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/forms"),require("@tailwindcss/typography"), require("daisyui")],
 
   daisyui: {
     themes: [
